d6798639c0079b447e40f5fb59166b9df88a4cfb
Luizwinter <junior.winter@gmail.com>
2021-04-29 15:27:31 -0300

    40. G12 - O operador Spread

---
+++ controllers/UserController.js
@@ -0,0 +1,71 @@
+class UserController {
+    constructor(formId, tableId){
+        
+        this.formEl = document.getElementById(formId);
+        this.tableEl = document.getElementById(tableId);
+        
+        this.onSubmit();
+
+    }
+    
+    onSubmit(){
+                
+       this.formEl.addEventListener("submit", event =>{
+    event.preventDefault();
+           
+           
+           this.addLine(this.getValues());
+
+    
+});
+    }
+   
+    
+    
+    
+    
+    getValues(){
+        
+        let user = {};
+    
+        [...this.formEl.elements].forEach(function(field, index){
+    
+    if(field.name == "gender"){
+        if(field.checked){
+
+            user[field.name] = field.value;
+        
+        }} else{
+            user[field.name] = field.value;
+        }
+    } 
+);
+    
+    return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+                
+    }
+    
+    
+    
+    addLine(dataUser){
+    
+    this.tableEl.innerHTML = `
+                  <tr>
+                    <td>
+                    <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm">
+                    </td>
+                    <td>${User.name}</td>
+                    <td>${User.email}</td>
+                    <td>${User.admin}</td>
+                    <td>${User.birth}</td>
+                    <td>
+                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    </td>
+                  </tr>`
+    ;
+}
+
+    
+    
+}//ClassClose
