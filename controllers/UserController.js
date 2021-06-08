6467e91c903639024c05ed94d190220430de7563
Luizwinter <junior.winter@gmail.com>
2021-06-08 12:29:42 -0300

    50. G22 - Trabalhando com Edição de Dados - Utilizando o laço For In para objetos

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,16 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit()
-
+        this.onEditCancel();
+    }
+    
+    onEditCancel(){
+        
+        document.querySelector('#box-user-update .btn-cancel').addEventListener('click', e=>{
+           
+            this.showPanelcreate();
+            
+        });
     }
 
     onSubmit(){
@@ -148,15 +157,35 @@ class UserController {
                 <td>${(dataUser.admin)? 'Sim' : 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
                 `;
-    
+        
+        tr.querySelector('.btn-edit').addEventListener('click', e=> {
+            
+            console.log(JSON.parse(tr.dataset.user));
+
+            this.showPanelUpdate();
+        
+        });
+        
         this.tableEl.appendChild(tr);
         
         this.updateCount();
     }
+    
+    showPanelcreate(){
+            document.querySelector("#box-user-create").style.display = 'block';
+            document.querySelector("#box-user-update").style.display = 'none';
+    }
+    
+    showPanelUpdate(){
+        
+            document.querySelector("#box-user-create").style.display = 'none';
+            document.querySelector("#box-user-update").style.display = 'block';        
+    }
+    
     
     updateCount(){
         
